8ae2bcfb6231e7d52ef661c3f51beab4c9bd8be5
lapridunshik <collapse@pnc.ru>
2012-11-27 13:49:30 +0400

    Version 10
    SmartRF regs

--- global_var.c
+++ global_var.c
@@ -70,24 +70,24 @@ __root const U8   MonthSize[12]=     { 31,28,31,30,31,30,31,31,30,31,30,31 };
 // GDO0 signal selection = ( 6) Asserts when sync word has been sent / received, and de-asserts at the end of the packet
 // GDO2 signal selection = (41) RF_RDY
 RF_SETTINGS rfSettings = {
-    0x08,   // FSCTRL1   Frequency synthesizer control.
+ 0x06,   // FSCTRL1   Frequency synthesizer control.
     0x00,   // FSCTRL0   Frequency synthesizer control.
     0x10,   // FREQ2     Frequency control word, high byte.
     0xA7,   // FREQ1     Frequency control word, middle byte.
     0x62,   // FREQ0     Frequency control word, low byte.
-    0xC5,   // MDMCFG4   Modem configuration.
+    0xF5,   // MDMCFG4   Modem configuration.
     0x83,   // MDMCFG3   Modem configuration.
-    0x93,   // MDMCFG2   Modem configuration.
+    0x13,   // MDMCFG2   Modem configuration.
     0x22,   // MDMCFG1   Modem configuration.
     0xF8,   // MDMCFG0   Modem configuration.
     0x00,   // CHANNR    Channel number.
-    0x34,   // DEVIATN   Modem deviation setting (when FSK modulation is enabled).
+    0x15,   // DEVIATN   Modem deviation setting (when FSK modulation is enabled).
     0x56,   // FREND1    Front end RX configuration.
     0x10,   // FREND0    Front end TX configuration.
-    0x18,   // MCSM0     Main Radio Control State Machine configuration.
+    0x10,   // MCSM0     Main Radio Control State Machine configuration.
     0x16,   // FOCCFG    Frequency Offset Compensation Configuration.
     0x6C,   // BSCFG     Bit synchronization Configuration.
-    0x43,   // AGCCTRL2  AGC control.
+    0x03,   // AGCCTRL2  AGC control.
     0x40,   // AGCCTRL1  AGC control.
     0x91,   // AGCCTRL0  AGC control.
     0xE9,   // FSCAL3    Frequency synthesizer calibration.
@@ -100,7 +100,7 @@ RF_SETTINGS rfSettings = {
     0x09,   // TEST0     Various test settings.
     0x47,   // FIFOTHR   RXFIFO and TXFIFO thresholds.
  //   0x29,   // IOCFG2    GDO2 output pin configuration.
- //   0x06,   // IOCFG0    GDO0 output pin configuration. Refer to SmartRF® Studio User Manual for detailed pseudo register explanation.
+//  0x06,   // IOCFG0    GDO0 output pin configuration. Refer to SmartRF® Studio User Manual for detailed pseudo register explanation.
     0x04,   // PKTCTRL1  Packet automation control.
     0x04,   // PKTCTRL0  Packet automation control.
     0x00,   // ADDR      Device address.
